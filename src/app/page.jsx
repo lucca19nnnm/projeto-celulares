b85cb6fd7ffb0ba2d482b1c3fc4d221943a77670
André Lucca <210523238+lucca19nn@users.noreply.github.com>
2025-08-06 23:48:09 -0300

    colocando os cards um do lado do outro

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,20 +1,21 @@
-  import ProductCard from '../components/ProductCard/ProductCard';
+import ProductCard from '../components/ProductCard/ProductCard';
+import styles from '../app/page.module.css'; // Certo
+import './globals.css';
 
-  export default function Page() {
-    return (
-      <main>
-        <h1>Produtos🔥</h1>
-        <ProductCard 
-          image="/image/iphone13.jpeg"
-          name="IPHONE 13"
-          price="3.499,99"
-        />
-
-        <ProductCard 
-          image="/image/iphone15.jpeg"
-          name="IPHONE 15"
-          price="4.899,99"
-        />
-      </main>
-    );
-  }
+export default function Page() {
+  return (
+    <main className={styles.main}> 
+      <h1 className={styles.title}>Produtos🔥</h1>
+      <ProductCard 
+        image="/image/iphone13.jpeg"
+        name="IPHONE 13"
+        price="3.499,99"
+      />
+      <ProductCard 
+        image="/image/iphone15.jpeg"
+        name="IPHONE 15"
+        price="4.899,99"
+      />
+    </main>
+  );
+}
